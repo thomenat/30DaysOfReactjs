bdf18b2b307e30b27043da72f223252c526d2ffc
˜nthome˜ <nataliathome@gmail.com>
2025-02-18 21:59:37 +0000

    added basic css

--- Day 08/edit-profile/src/EditProfile.jsx
+++ Day 08/edit-profile/src/EditProfile.jsx
@@ -1,45 +1,52 @@
 import React from 'react'
-
+import './EditProfile.css'
 const EditProfile = () => {
   return (
-    <div className='EditProfile-container flex'> 
+    <div className='container'> 
     <h1>Edit Profile</h1>
+    <div className='name'>
     <input 
     type='text' 
     name='name'
     placeholder='Name'
     required />
+    </div>
 
+    <div className='email'>
     <input 
     type='email'
     name='email'
     placeholder='Email'
     required/>
+    </div>
 
+    <div className='bio'>
     <textarea 
     type='text'
     name='bio'
     placeholder='Short bio'
     />
+    </div>
 
+    <div className='theme'>
     <select 
     name='theme'
-    required></select>
+    required>
+        <option value='light'>Light Theme</option>
+        <option value='dark'>Dark Theme</option>
+    </select>
+    </div>
 
+    <div className='avatar'>
     <input 
     type='file' 
     name='avatar'
     accept='image/*'
     />
+    </div>
     
 
     <button type='submit'>Save Changes</button>
-
-
-
-
-
-
     </div>
   )
 }
