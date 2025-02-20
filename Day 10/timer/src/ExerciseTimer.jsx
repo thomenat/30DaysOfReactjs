288e6396e30ee09d4f52eb3dcf3939e2a9c00a6f
˜nthome˜ <nataliathome@gmail.com>
2025-02-20 20:29:52 +0000

    added constructor and mounting fuctions

--- Day 10/timer/src/ExerciseTimer.jsx
+++ Day 10/timer/src/ExerciseTimer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 
 class ExerciseTimer extends React.Component {
+  
+  const [seconds, setSeconds] = useState(0);
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 30
+    };
+  }
+  
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.state.seconds > 0) {
+        this.setState(prevState => ({
+          seconds: prevState.seconds - 1
+        }));
+      } else {
+        clearInterval(this.interval);
+      }, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  
+  
+
+
   render() {
     return (
       <div>
-        {/* Timer UI will go here */}
+        <h2>{this.state.seconds} seconds left</h2>
       </div>
     );
   }
