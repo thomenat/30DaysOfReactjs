802a9663861eac40104b2c983495b5d2f5855672
˜nthome˜ <nataliathome@gmail.com>
2025-02-20 20:04:14 +0000

    created day 10 basic react structure

---
+++ Day 10/timer/src/ExerciseTimer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ExerciseTimer extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Timer UI will go here */}
+      </div>
+    );
+  }
+}
+
+export default ExerciseTimer;
