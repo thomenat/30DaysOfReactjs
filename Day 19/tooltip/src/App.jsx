3719538f6d29d35d0100c968bbbc7b6c23ad55f6
˜nthome˜ <nataliathome@gmail.com>
2025-03-17 22:27:33 +0000

    added an extra button and improved style

--- Day 19/tooltip/src/App.jsx
+++ Day 19/tooltip/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import './App.css'
 import ButtonWithTooltip from './ButtonWithTooltip'
 
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px", alignItems: "center", marginTop: "50px" }}>
       <h1>React Portals: Tooltip</h1>
-      <ButtonWithTooltip />
+      <ButtonWithTooltip text="Hello! I am a tooltip." />
+      <ButtonWithTooltip text="Another tooltip example!" />
     </div>
   );
 }
+
 
 
 export default App
--- Day 19/tooltip/src/ButtonWithTooltip.jsx
+++ Day 19/tooltip/src/ButtonWithTooltip.jsx
@@ -2,21 +2,32 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 
-const ButtonWithTooltip = () => {
+const ButtonWithTooltip = ({ text }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
+  const handleMouseLeave = () => {
+    setTimeout(() => {
+      setShowTooltip(false);
+    }, 300);
+  };
+  
+  
+  
   return (
+    <div style={ { position: "relative"}}>
     <button
       onMouseEnter={() => setShowTooltip(true)}  // Show tooltip on hover
-      onMouseLeave={() => setShowTooltip(false)} // Hide tooltip when not hovered
+      onMouseLeave={() => handleMouseLeave && setShowTooltip(false)} // Hide tooltip when not hovered
     >
       Hover me!
+      </button>
+
       {showTooltip &&
   ReactDOM.createPortal(
-    <div className="tooltip">This is a tooltip</div>,
+    <div className="tooltip">{text}</div>,
     document.getElementById("portal-root")
   )}
-    </button>
+  </div>
   );
 };
 
