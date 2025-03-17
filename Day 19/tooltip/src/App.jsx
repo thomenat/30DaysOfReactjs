a979edd7f006457116097724ba7a53d0db0ecff2
˜nthome˜ <nataliathome@gmail.com>
2025-03-17 17:12:26 +0000

    made tooltip a portal

--- Day 19/tooltip/src/App.jsx
+++ Day 19/tooltip/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import ButtonWithTooltip from './ButtonWithTooltip'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>React Portals: Tooltip</h1>
+      <ButtonWithTooltip />
+    </div>
+  );
 }
+
 
 export default App
--- Day 19/tooltip/src/ButtonWithTooltip.jsx
+++ Day 19/tooltip/src/ButtonWithTooltip.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-export default function ButtonWithTooltip() {
+
+const ButtonWithTooltip = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
   return (
-    <button>
-        Click
+    <button
+      onMouseEnter={() => setShowTooltip(true)}  // Show tooltip on hover
+      onMouseLeave={() => setShowTooltip(false)} // Hide tooltip when not hovered
+    >
+      Hover me!
+      {showTooltip &&
+  ReactDOM.createPortal(
+    <div className="tooltip">This is a tooltip</div>,
+    document.getElementById("portal-root")
+  )}
     </button>
-  )
-}
+  );
+};
+
+export default ButtonWithTooltip;
